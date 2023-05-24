e0bbf91e7cad9780185da999644f4126a7ddff98
osama.b.hamed <osama.b.hamed@gmail.com>
2023-05-24 22:56:27 +0300

    add

---
+++ testtask/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using testtask.Models;
+
+namespace testtask.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmailSender emailSender;
+
+        public EmployeeController(IEmailSender emailSender)
+        {
+            this.emailSender = emailSender;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            await emailSender.SendEmailAsync("ahmad@gmail.com", "subject", "message");
+
+            return View();
+        }
+
+        public IActionResult save()
+        {
+            // save
+            return Content("success");
+        }
+    }
+}
---
+++ testtask/Models/EmailSender.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace testtask.Models
+{
+    public class EmailSender: IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                EnableSsl = true,
+                UseDefaultCredentials = true,
+                Credentials = new NetworkCredential("shima@gmail.com", "mvydsijnkgvwtwnp")
+            };
+
+            return client.SendMailAsync(
+                new MailMessage(from: "shima@gmail.com",
+                                to: email,
+                                subject,
+                                message
+                                ));
+        }
+    }
+}
